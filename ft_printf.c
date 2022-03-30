10e520607197abf08eadbf60a6f60b20919a9a85
Theo Kempf elbaz <tkempf-e@k0r2p4.42mulhouse.fr>
2022-03-30 15:29:04 +0200

    maj

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: tkempf-e <tkempf-e@student.42mulhouse.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/25 10:27:39 by tkempf-e          #+#    #+#             */
-/*   Updated: 2022/03/30 11:23:47 by tkempf-e         ###   ########.fr       */
+/*   Updated: 2022/03/30 15:28:57 by tkempf-e         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,6 +40,32 @@ int	ft_strlen(const char *s)
 	while (s[i])
 		i++;
 	return (i);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	char	*join;
+	int		i;
+	int		j;
+
+	j = 0;
+	i = 0;
+	join = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (!join)
+		return (NULL);
+	while (s1 && s1[i])
+	{
+		join[i] = *(char *)(s1 + i);
+		i++;
+	}
+	while (s2[j])
+	{
+		join[i + j] = *(char *)(s2 + j);
+		j++;
+	}
+	join[i + j] = '\0';
+	free(s1);
+	return (join);
 }
 
 void	*ft_memcpy(void *dest, const void *src, size_t n)
@@ -77,10 +103,87 @@ char	*ft_charjoin(char *tab, char c)
 	return (str);
 }
 
+char	*ft_strdup(const char *s)
+{
+	char	*cpy;
+
+	cpy = malloc(ft_strlen(s) * sizeof(char) + 1);
+	if (!cpy)
+		return (0);
+	ft_memcpy(cpy, s, ft_strlen(s) * sizeof(char) + 1);
+	return (cpy);
+}
+
+long int	ft_numbers(long int n)
+{
+	long int	numbers;
+
+	numbers = 1;
+	if (n == 0)
+		numbers = 2;
+	if (n < 0)
+		numbers++;
+	while (n != 0)
+	{
+		n /= 10;
+		numbers++;
+	}
+	return (numbers);
+}
+
+char	*filling(long int n, char *tab, long int j, long int numbers)
+{
+	if (n == 0)
+	{
+		tab[j] = 48;
+		tab[j + 1] = '\0';
+		return (tab);
+	}
+	while (n > 0)
+	{
+		numbers = n / 10;
+		tab[j] = n - (numbers * 10) + 48;
+		n /= 10;
+		j--;
+	}
+	return (tab);
+}
+
+char	*ft_itoa(int n, char *str)
+{
+	char		*tab;
+	long int	i;
+	long int	j;
+	long int	numbers;
+
+	i = 0;
+	if (n == -2147483648)
+		return (ft_strdup("-2147483648"));
+	numbers = ft_numbers((long int)n);
+	tab = malloc(sizeof(char) * numbers);
+	if (!tab)
+		return (NULL);
+	if (n < 0)
+	{
+		tab[i] = '-';
+		i++;
+		n *= -1;
+	}
+	j = numbers - 2;
+	tab[numbers - 1] = '\0';
+	tab = filling((long int)n, tab, j, numbers);
+	tab = ft_strjoin(str, tab);
+	return (tab);
+}
+
 char	*ft_printf_options(char *tab, const char *str, int i, va_list ptr)
 {
 	if (str[i + 1] == 'c')
-		tab = ft_charjoin(tab, va_arg(ptr, char));
+		tab = ft_charjoin(tab, va_arg(ptr, int));
+	else if (str[i + 1] == 's')
+		tab = ft_strjoin(tab, va_arg(ptr, char *));
+	else if (str[i + 1] == 'd')
+		tab = ft_itoa(va_arg(ptr, int), tab);
 	return (tab);
 }
 
@@ -117,28 +220,10 @@ int	main(void)
 {
 	// int	nbr = 4;
 	char	c;
+	char	s[] = "pommes et des";
+	int		d = 6;
 	
 	c = 'F';
-	ft_printf("\nmanger des p%cates\n", c);
+	ft_printf("\nmanger des %s p%cates %d fois par semaine\n", s, c, d);
 	return (0);
 }
-
-// else if (str[i + 1] == 's')
-// 			ft_joinstr
-// 		else if (str[i + 1] == 'p')
-// 			ft_joinptr
-// 		else if (str[i + 1] == 'd')
-// 			ft_joinnbr
-// 		else if (str[i + 1] == 'i')
-// 			ft_joinnbr
-// 		else if (str[i + 1] == 'u')
-// 			ft_joinnbr
-// 		else if (str[i + 1] == 'x')
-// 			ft_joinhexamin
-// 		else if (str[i + 1] == 'X')
-// 			ft_joinhexamaj
-// 		else if (str[i + 1] == '%')//passe les deux '%'
-// 	{
-// 		tab = ft_stradd(tab, str[i])
-// 		i++;
-// 	}
